818a2bf94f1d3f9338384b10fabdc63c9dde7b0a
Jake Kirkbride <JakeLegendXIII@gmail.com>
2022-11-29 14:36:11 -0500

    Adding seeded DB fixture and retire a course test

---
+++ courses/src/CoursesApi.IntegrationTests/CoursesResource/GettingCoursesWithData.cs
@@ -0,0 +1,37 @@
+﻿
+using Alba;
+
+using CoursesApi.Models;
+
+namespace CoursesApi.IntegrationTests.CoursesResource;
+
+public class GettingCoursesWithData : IClassFixture<CoursesSeededResourceFixture>
+{
+    private readonly IAlbaHost _host;
+    public GettingCoursesWithData(CoursesSeededResourceFixture fixture)
+    {
+        _host = fixture.AlbaHost;
+    }
+
+    [Fact]
+    public async Task GettingTheInitialCourseList()
+    {
+        // when I do a GET /courses
+        // I get a 200 OK Status Response
+
+        var response = await _host.Scenario(api =>
+        {
+            api.Get.Url("/courses");
+            api.StatusCodeShouldBeOk();
+        });
+
+        var entity = await response.ReadAsJsonAsync<CoursesResponseModel>();
+
+        entity = entity is not null ? entity : throw new ArgumentNullException(nameof(entity));
+
+        Assert.Equal(1, entity.NumberOfFrontendCourses);
+        Assert.Equal(2, entity.NumberOfBackendCourses);
+        //whatever you feel you need to do here..
+        Assert.Equal(3, entity.Courses.Count);
+    }
+}
---
+++ courses/src/CoursesApi.IntegrationTests/CoursesResource/RetiringACourse.cs
@@ -0,0 +1,39 @@
+﻿
+using Alba;
+
+namespace CoursesApi.IntegrationTests.CoursesResource;
+
+public class RetiringACourse : IClassFixture<CoursesSeededResourceFixture>
+{
+    private readonly IAlbaHost _host;
+
+    public RetiringACourse(CoursesSeededResourceFixture fixture)
+    {
+        _host = fixture.AlbaHost;
+    }
+
+    [Fact]
+    public async Task CanRetireACourse()
+    {
+        await _host.Scenario(api =>
+        {
+            api.Get.Url("/courses/1");
+            api.StatusCodeShouldBeOk();
+        });
+
+        await _host.Scenario(api =>
+        {
+            // just delete courses/1
+            api.Delete.Url("/courses/1");
+            api.StatusCodeShouldBe(204);
+        });
+
+        await _host.Scenario(api =>
+        {
+            api.Get.Url("/courses/1");
+            api.StatusCodeShouldBe(404);
+        });        
+
+
+    }
+}
